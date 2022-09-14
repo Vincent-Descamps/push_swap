e026923a73167159d61393f19587c72f24022dd5
Vincent Descamps <vincedescamps@gmail.com>
2022-09-14 16:06:22 +0200

    algo ok - errors left

--- srcs/big_algo.c
+++ srcs/big_algo.c
@@ -6,11 +6,27 @@
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:51:22 by vdescamp          #+#    #+#             */
-/*   Updated: 2022/09/13 15:15:33 by vdescamp         ###   ########.fr       */
+/*   Updated: 2022/09/14 15:53:52 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/push_swap.h"
+
+void	chunk_1(t_list **list_a, t_list **list_b, int last_index, int med, int len, int x)
+{
+	int	chunk;
+	int	frac;
+
+	frac = len / 5;
+	frac = x * frac;
+	chunk = len - (frac);
+	while ((list_len(*list_b) < frac - 2))
+	{
+		while ((*list_a)->index >= chunk && (*list_a)->index != last_index && (*list_a)->index != med)
+			execute(list_a, list_b, 1, "pb");
+		execute(list_a, list_b, 1, "ra");
+	}
+}
 
 void	finish(t_list **list_a, t_list **list_b)
 {
@@ -20,34 +36,27 @@ void	finish(t_list **list_a, t_list **list_b)
 
 void	part_2(t_list **list_a, t_list **list_b)
 {
-	int	len;
-	int	med;
-
-	len = list_len(*list_b) - 1;
-	med = len / 2;
 	execute(list_a, list_b, 1, "ra");
 	execute(list_a, list_b, 1, "pa");
-	while ((*list_b)->next != NULL)
+	while ((*list_b) != NULL)
 	{
 		if ((*list_b)->index < (*list_a)->index)
 		{
 			if ((*list_b)->index > get_last_elem(*list_a)->index)
 			{
 				execute(list_a, list_b, 1, "pa");
-				//len--;
 			}
 			else if ((*list_b)->index < get_last_elem(*list_a)->index)
 				execute(list_a, list_b, 1, "rra");
 		}
-		if ((*list_b)->index > (*list_a)->index)
+		else if ((*list_b)->index > (*list_a)->index)
 		{
 			while ((*list_b)->index > (*list_a)->index)
 				execute(list_a, list_b, 1, "ra");
 			execute(list_a, list_b, 1, "pa");
-			//len--;
 		}
 	}
-	if ((*list_b)->index > (*list_a)->index)
+	/*if ((*list_b)->index > (*list_a)->index)
 	{
 		while ((*list_b)->index > (*list_a)->index)
 			execute(list_a, list_b, 1, "ra");
@@ -57,7 +66,7 @@ void	part_2(t_list **list_a, t_list **list_b)
 		while ((*list_b)->index < (*list_a)->index)
 			execute(list_a, list_b, 1, "rra");
 	}
-	execute(list_a, list_b, 1, "pa");
+	execute(list_a, list_b, 1, "pa");*/
 }
 
 void	big_algo(t_list **list_a, t_list **list_b)
@@ -65,35 +74,36 @@ void	big_algo(t_list **list_a, t_list **list_b)
 	int		med;
 	int		len;
 	int		last_index;
-	//t_list	*tmp;
+	int		x = 1;
 
-	//tmp = *list_b;
 	len = list_len(*list_a);
 	med = (len / 2);
 	last_index = len - 1;
 	index_list(*list_a);
-	while (len > 3)
+	/*chunk_1(list_a, list_b, last_index, len, 1);
+	chunk_1(list_a, list_b, last_index, len, 2);
+	chunk_1(list_a, list_b, last_index, len, 3);
+	chunk_1(list_a, list_b, last_index, len, 4);*/
+	while (list_len(*list_a) > 3)
 	{
-		while (len > med + 3)
+		while(x < 5)
 		{
-			if ((*list_a)->index == 0 || (*list_a)->index == med || (*list_a)->index == last_index)
-				execute(list_a, list_b, 1, "ra");
-			if ((*list_a)->index > med)
-			{
-				execute(list_a, list_b, 1, "pb");
-				len--;
-			}
-			else if (((*list_a)->index < med))
-				execute(list_a, list_b, 1, "ra");
+			chunk_1(list_a, list_b, last_index, med, len, x);
+			x++;
 		}
-		if ((*list_a)->index == 0 || (*list_a)->index == med || (*list_a)->index == last_index)
+		if ((*list_a)->index == 0 || (*list_a)->index == last_index || (*list_a)->index == med)
 			execute(list_a, list_b, 1, "ra");
 		else
-		{
 			execute(list_a, list_b, 1, "pb");
-			len--;
-		}
 	}
+	//chunk_1(list_a, list_b, last_index, 3);
+	//chunk_1(list_a, list_b, last_index, len, 2);
+	/*while (list_len(*list_a) > 3)
+	{
+		if (!((*list_a)->index == 0 || (*list_a)->index == med || (*list_a)->index == last_index))
+			execute(list_a, list_b, 1, "pb");
+		execute(list_a, list_b, 1, "ra");
+	}*/
 	small_algo(list_a, list_b);
 	part_2(list_a, list_b);
 	finish(list_a, list_b);
